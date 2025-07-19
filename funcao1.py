9e1a2057dabeaed15cfc7c4d69c061d176eb7d96
Tuta-Info-e-Cia <tutainfoecia@gmail.com>
2025-07-18 21:49:25 -0300

    Add files via upload

---
+++ funcao1.py
@@ -0,0 +1,21 @@
+def somar(a, b):
+    print("Resultado => ", a+b)
+    
+def somar1(a,b,c):
+    print("Resultado => ", a+b+c)
+    
+def subtrair(x,y):
+    print("Resultado =>", x-y)
+        
+def multiplicar(a,b):
+    print("Resultado => ", a*b)
+    
+def dividir(a,b):
+    print("Resultado => ",a/b)
+    
+#chamada da função
+somar(3,4)
+subtrair(3,5)
+multiplicar(5,5)
+dividir(12,2)
+
